af03ca31140bb3611a649e3811132fef6e7acdb5
Daniel Backes <danielbackes@protonmail.com>
2021-08-03 17:34:05 -0400

    feat: add validation to file upload

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -16,6 +16,8 @@ use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class ArticleFormType extends AbstractType
 {
@@ -51,9 +53,25 @@ class ArticleFormType extends AbstractType
                 ],
                 'required' => false,
             ])
+        ;
+
+        $imageConstraints = [
+            new Image([
+                'maxSize' => '5M'
+            ]),
+        ];
+
+        if (!$isEdit || !$article->getImageFilename()) {
+            $imageConstraints[] = new NotNull([
+                'message' => 'Please upload an image'
+            ]);
+        }
+
+        $builder
             ->add('imageFile', FileType::class, [
                 'mapped' => false,
                 'required' => false,
+                'constraints' => $imageConstraints
             ])
         ;
 
