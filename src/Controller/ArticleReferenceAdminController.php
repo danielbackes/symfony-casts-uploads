5963fdd623ffce5cc109f06b6882adf584096f21
Daniel Backes <danielbackes@protonmail.com>
2021-08-27 07:32:59 -0400

    feat: change the download action to return directly the S3 signed url

--- src/Controller/ArticleReferenceAdminController.php
+++ src/Controller/ArticleReferenceAdminController.php
@@ -5,13 +5,16 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Entity\ArticleReference;
 use App\Service\UploaderHelper;
+use Aws\S3\S3Client;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\HeaderUtils;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Constraints\File;
@@ -127,27 +130,26 @@ class ArticleReferenceAdminController extends BaseController
     /**
      * @Route("/admin/article/references/{id}/download", name="admin_article_download_reference", methods={"GET"})
      */
-    public function downloadArticleReference(ArticleReference $reference, UploaderHelper $uploaderHelper)
+    public function downloadArticleReference(ArticleReference $reference, S3Client $s3Client, string $s3BucketName)
     {
       $article = $reference->getArticle();
       $this->denyAccessUnlessGranted('MANAGE', $article);
 
-      $response = new StreamedResponse(function() use ($reference, $uploaderHelper) {
-        $outputStream = fopen('php://output', 'wb');
-        $fileStream = $uploaderHelper->readStream($reference->getFilePath());
-
-        stream_copy_to_stream($fileStream, $outputStream);
-      });
-
-      $response->headers->set('Content-Type', $reference->getMimeType());
-
       $disposition = HeaderUtils::makeDisposition(
-        HeaderUtils::DISPOSITION_ATTACHMENT,
+        ResponseHeaderBag::DISPOSITION_ATTACHMENT,
         $reference->getOriginalFilename()
       );
-      $response->headers->set('Content-Disposition', $disposition);
 
-      return $response;
+      $command = $s3Client->getCommand('GetObject', [
+        'Bucket' => $s3BucketName,
+        'Key' => $reference->getFilePath(),
+        'ResponseContentType' => $reference->getMimeType(),
+        'ResponseContentDisposition' => $disposition,
+      ]);
+      
+      $request = $s3Client->createPresignedRequest($command, '+30 minutes');
+      
+      return new RedirectResponse((string) $request->getUri());
     }
 
     /**
